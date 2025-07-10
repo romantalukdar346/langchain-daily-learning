7c8c092bd864e65ec9bb7430073b432cb6b275ad
Roman Talukdar <93270252+romantalukdar346@users.noreply.github.com>
2025-07-11 03:13:05 +0600

    Update 1_chatmodel_openai.py

--- 2.ChatModels/1_chatmodel_openai.py
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -1,10 +1,10 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
 load_dotenv()
 
-# model= ChatOpenAI(model="gpt-3.5-turbo")
-model = ChatOllama(model="llama3.2")
+model= ChatOpenAI(model="gpt-3.5-turbo")
+# model = ChatOllama(model="llama3.2")
 
 res= model.invoke("What is the capital of Bangladesh?")
 
